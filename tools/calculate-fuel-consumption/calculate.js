026cc545a5349df853731dad1642bd3ad3cf7dfb
Oleg Artemiev <oartemjev@gmail.com>
2023-07-03 20:19:07 +0300

    format clipboard string

--- tools/calculate-fuel-consumption/calculate.js
+++ tools/calculate-fuel-consumption/calculate.js
@@ -6,21 +6,26 @@ const clculateButton = document.getElementById('calculate');
 const result = document.getElementById('result');
 
 const calculateFuelConsumption = (distanceInMiles, fuelSuppliedInLiters) => {
-    console.log(distanceInMiles);
-    console.log(fuelSuppliedInLiters);
-
     const distanceInKilometers = distanceInMiles * 1.609344;
     const fuelSupplied = fuelSuppliedInLiters / distanceInKilometers * 100;
     return fuelSupplied.toFixed(2);
+};
+
+const getClipboardString = (fuelConsumption, distanceInMiles, fuelSuppliedInLiters) => {
+    const currentDate = new Date().toLocaleDateString('de-DE');
+    const clipboardString = `${currentDate}
+${distanceInMiles} miles; ${fuelSuppliedInLiters} liters;
+Fuel consumption: ${fuelConsumption} Liters/100km`;
+    return clipboardString;
 }
 
-clculateButton.addEventListener('click', function() {
+clculateButton.addEventListener('click', function () {
     const distanceInMiles = distanceInMilesInput.value;
     const fuelSuppliedInLiters = fuelSuppliedInLitersInput.value;
     const fuelConsumption = calculateFuelConsumption(distanceInMiles, fuelSuppliedInLiters);
     result.innerHTML = fuelConsumption;
-        const resultString = document.getElementById('result-string').innerText;
-    navigator.clipboard.writeText(resultString);
+    const clipboardString = getClipboardString(fuelConsumption, distanceInMiles, fuelSuppliedInLiters);
+    navigator.clipboard.writeText(clipboardString);
 });
 
 distanceInMilesInput.onkeyup = () => {
